0db298f012ad78ed043f6ae78527b5f96d93b972
Saeid Mirzaei <saeid.mirzaei@atos.net>
2014-11-27 16:26:23 +0100

    final code. Test passes according to requirement

---
+++ src/test/java/org/activiti/DynaTaskService.java
@@ -0,0 +1,10 @@
+package org.activiti;
+
+import org.activiti.engine.impl.persistence.entity.ExecutionEntity;
+
+public interface DynaTaskService  {
+	public void createOneTask(ExecutionEntity execution);
+
+	public void createTwoTasks(ExecutionEntity execution);
+
+}
--- src/test/java/org/activiti/MyUserTaskActiviyBehavior.java
+++ src/test/java/org/activiti/MyUserTaskActiviyBehavior.java
@@ -12,30 +12,49 @@ import org.activiti.engine.impl.pvm.runtime.AtomicOperation;
 import org.activiti.engine.impl.task.TaskDefinition;
 
 public class MyUserTaskActiviyBehavior extends UserTaskActivityBehavior {
+	DynamicTaskTool dynamicTaskTool = new DynamicTaskTool();
 
 	public MyUserTaskActiviyBehavior(TaskDefinition taskDefinition) {
 		super(taskDefinition);
 		// TODO Auto-generated constructor stub
 	}
 	
+	private void increaseCompletedNumber(ActivityExecution execution) {
+		   Integer completed = (Integer) execution.getVariable(DynamicTaskTool.DYNATASK_COMPLETERD_STRING);
+		   if (completed == null)
+			   completed = 0;
+		   execution.setVariable(DynamicTaskTool.DYNATASK_COMPLETERD_STRING, completed + 1);
+		   
+		   
+		
+	}
+	
 	  public void signal(ActivityExecution execution, String signalName, Object signalData) throws Exception {
 		  
 		  RuntimeService runtimeService = execution.getEngineServices().getRuntimeService();
- 		    System.out.println("Ha ha my signal at last. Task name =" + execution.getActivity().getProperty("testProperty") + " isDynamic=" + execution.getVariable("isDynamic"));
-		   System.out.println("execution id =" + execution.getId());
+		  String activityName = execution.getActivity().getId();
+		  String command = (String) execution.getVariable(DynamicTaskTool.DYNATASK_COMMAND_VAR);
+ 		    System.out.println("Task signalled. Task name =" + activityName + " execution id=" + execution.getId());
+ 		    
+ 		    boolean isDynaTask = activityName.equals("oneStepTask") || activityName.equals("twoStepTaskHead") ||activityName.startsWith(DynamicTaskTool.DYNATASK_PREFIX);
 		   
-		   List<PvmTransition> outgoingTransitions = execution.getActivity().getOutgoingTransitions();
+	
+			   List<PvmTransition> outgoingTransitions = execution.getActivity().getOutgoingTransitions();
+		   
 		   if (outgoingTransitions.size() == 0) {
-			   ActivityExecution parentExecution = execution.getParent();
-			   System.out.println("this is an end task. Do Nothing");
-			   ((ExecutionEntity) execution).setScope(false);
-			   ((ExecutionEntity) execution).performOperation(AtomicOperation.ACTIVITY_END);
-			   runtimeService.signal(parentExecution.getId());   
+			   
+				   ActivityExecution parentExecution = execution.getParent();
+				   System.out.println("this is an end task.");
+				   increaseCompletedNumber(execution);
+				   ((ExecutionEntity) execution).setScope(false);
+				   ((ExecutionEntity) execution).performOperation(AtomicOperation.ACTIVITY_END);
+				   runtimeService.signal(parentExecution.getId());
+				   
 			   }
 		   else
 		    if (!((ExecutionEntity)execution).getTasks().isEmpty())
 		      throw new ActivitiException("UserTask should not be signalled before complete");
-		    leave(execution);
+			   leave(execution);
 		  }
 
 
